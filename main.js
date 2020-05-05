8f310173258e04f8685c72c31d1a2841c3d15d47
Maciej Rapacki <mac.rapacki@gmail.com>
2020-05-05 20:10:14 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const head = document.querySelector('.heads')
+const tail = document.querySelector('.tails')
+const choice = document.querySelector('p span.choice')
+
+const headBtn = () => {
+    choice.innerHTML = 'Heads'
+}
+
+const tailBtn = () => {
+    choice.innerHTML = 'Tails'
+}
+
+head.addEventListener('click', headBtn)
+tail.addEventListener('click', tailBtn)
+
+///////////////////////////////////////////
+
+const playBtn = document.querySelector('.play')
+const winNumber = document.querySelector('p.win span')
+const losNumber = document.querySelector('p.lost span')
+const totalGames = document.querySelector('p.total span')
+
+const arr = ['Heads', 'Tails']
+
+
+const winCheck = () => {
+    const indexNumber = (Math.floor(Math.random() * arr.length));
+
+    if (choice.innerHTML === arr[indexNumber]) {
+        totalGames.innerHTML++;
+        winNumber.innerHTML++;
+        choice.innerHTML = '';
+
+    } else {
+        totalGames.innerHTML++;
+        losNumber.innerHTML++;
+        choice.innerHTML = '';
+
+    }
+}
+
+playBtn.addEventListener('click', winCheck)
